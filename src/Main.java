4895adc1bbb45ffbe1ef17da7e5c57dfa6596c54
harindu <DELL@Win10>
2021-11-07 23:46:38 +0530

    adding comments on main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		adminpanel panel1 = new adminpanel();
+		adminpanel panel1 = new adminpanel();//good bye world 
+		//hello world
 
 	}
 
